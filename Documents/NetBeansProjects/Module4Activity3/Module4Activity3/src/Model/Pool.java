a40e2de5ad935b1c5a0229986e5733f769150556
Dmitry Mikhailov <dmikhailov@me.com>
2018-09-14 19:30:21 +0300

    Initial commit

---
+++ Documents/NetBeansProjects/Module4Activity3/Module4Activity3/src/Model/Pool.java
@@ -0,0 +1,45 @@
+package Model;
+
+import Model.Reports.Report;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author d.mikhaylov
+ */
+public class Pool {
+
+    private Report report;
+    private ArrayList<Swimmer> swimmers;
+    private ArrayList<Visit> visits;
+
+    public Pool() {
+        swimmers = new ArrayList<>();
+        visits = new ArrayList<>();
+        report = new Report(this);
+    }
+
+    /**
+     * @return the report
+     */
+    public Report getReport() {
+        return report;
+    }
+
+    /**
+     * @return the swimmers
+     */
+    public ArrayList<Swimmer> getSwimmers() {
+        return swimmers;
+    }
+
+    /**
+     * @return the visits
+     */
+    public ArrayList<Visit> getVisits() {
+        return visits;
+    }
+    
+    
+
+}
---
+++ Documents/NetBeansProjects/Module4Activity3/Module4Activity3/src/app.java
@@ -0,0 +1,19 @@
+
+import Model.Pool;
+
+
+
+/**
+ *
+ * @author d.mikhaylov
+ */
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pool pool = new Pool();
+    }
+    
+}
